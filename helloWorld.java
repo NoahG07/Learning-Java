e888afe8d8cff490fdcde7d8139943be7ce079d0
Noah Garcia <34785593+NoahG07@users.noreply.github.com>
2021-01-28 19:31:09 -0800

    Add files via upload

---
+++ helloWorld.java
@@ -0,0 +1,7 @@
+public class helloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello, World");
+		System.out.print("I am learning Java now. ");
+		System.out.print("It seems pretty cool.\n");
+	}
+}
